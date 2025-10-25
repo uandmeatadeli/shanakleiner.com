e15027f184df8185d11797d1d47f09836f44766f
Jack Arteaga <arteagajackCS@gmail.com>
2025-10-25 15:50:22 -0400

    Changed home page formatting / added new headshot / created individual css for pages / menu defualt open on home page / changed misc page to creative work and added video player for films

--- js/script.js
+++ js/script.js
@@ -1,21 +1,33 @@
-$(function(){
+$(function () {
 
     let menuPath = window.location.pathname.includes("/pages/") ? "../menu.html" : "menu.html";
+    let isMenuOpen = false;
 
-    $("#menu-container").load(menuPath, function(){
+    $("#menu-container").load(menuPath, function () {
         const menu = document.getElementById("side-menu");
         const menuBtn = document.getElementById("menu-btn");
-        const closeBtn = document.getElementById("close-btn");
+
+        // if we are on the home page have menu open by default
+        if (window.location.pathname === "/" || window.location.pathname.endsWith("index.html")) {
+            const menu = document.getElementById("side-menu");
+            if (menu) {
+                menu.style.width = "250px";
+                isMenuOpen = true;
+            }
+        }
 
         // Open Menu
         menuBtn.addEventListener("click", () => {
-            menu.style.width = "250px";
+            if (!isMenuOpen) {
+                menu.style.width = "250px";
+                isMenuOpen = true;
+            } else {
+                menu.style.width = "0";
+                isMenuOpen = false;
+            }
         });
 
-        // Close Menu
-        closeBtn.addEventListener("click", () => {
-            menu.style.width = "0";
-        });
     });
+
 });
 
